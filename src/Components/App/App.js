0136b0a0a4f296a292d6bf2c4be8bdc6e619ef7b
Erick Cristian dos Santos <erick.cristians@gmail.com>
2022-07-03 00:00:42 -0300

    Commit inicial

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import IfoodCounter from '../IfoodCounter/IfoodCounter';
+
+
+function App() {
+  return (
+    <>
+      <h1>Ifood</h1>
+      <IfoodCounter/>
+    </>
+  );
+}
+
+export default App;
